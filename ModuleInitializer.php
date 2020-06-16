3fb9d93cfc49fe66ed22c091ec677e708d45dd5f
JaviTerra <javier.terranova@gmail.com>
2020-06-16 10:43:02 -0300

    Guardo cambios Clima

--- ModuleInitializer.php
+++ ModuleInitializer.php
@@ -2,6 +2,7 @@
 require_once("helper/Renderer.php");
 include_once("helper/Database.php");
 include_once("helper/Config.php");
+include_once("helper/Clima.php");
 require_once('third-party/mustache/src/Mustache/Autoloader.php');
 
 
